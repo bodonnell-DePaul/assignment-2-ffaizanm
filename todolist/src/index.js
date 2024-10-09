412c8763b606f36a7a0f5d2e8bf42ceef6d8bc28
ffaizanm <146502900+ffaizanm@users.noreply.github.com>
2024-10-09 05:50:28 -0500

    Add files via upload

---
+++ todolist/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import App from './App'; // Import your App component
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root') // Matches the div in index.html
+);
